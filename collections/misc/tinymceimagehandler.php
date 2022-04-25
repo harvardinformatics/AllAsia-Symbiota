3bafebcebfa0492556c041b0b6463fd24907e4b5
Greg Post <gregory.post@asu.edu>
2022-04-25 18:50:09 -0500

    nightly commit

--- collections/misc/tinymceimagehandler.php
+++ collections/misc/tinymceimagehandler.php
@@ -1,20 +1,20 @@
 <?php
-
+include_once('../../config/symbini.php');
 /**
  * Based on code copied from https://www.tiny.cloud/docs/advanced/php-upload-handler/
  * modified by Greg Post
  */
-include_once('../../config/symbini.php');
 
 /***************************************************
  * Only these origins are allowed to upload images *
  ***************************************************/
-$accepted_origins = array("http://localhost",);
+$accepted_origins = array("http://localhost", "http://$SERVER_HOST", "https://$SERVER_HOST");
 
 /*********************************************
  * Change this line to set the upload folder *
  *********************************************/
-$imageFolder = "images/";
+$imageFolder = $SERVER_ROOT . $PUBLIC_IMAGE_UPLOAD_ROOT;
+$imageURL = $CLIENT_ROOT . $PUBLIC_IMAGE_UPLOAD_ROOT . '/';
 
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     // same-origin requests won't set an origin. If the origin is set, it must be valid.
@@ -65,7 +65,7 @@ if (is_uploaded_file($temp['tmp_name'])) {
     // Respond to the successful upload with JSON.
     // Use a location key to specify the path to the saved image resource.
     // { location : '/your/uploaded/image/file'}
-    echo json_encode(array('location' => $baseurl . $filetowrite));
+    echo json_encode(array('location' => $baseurl . $imageURL . $temp['name']));
 } else {
     // Notify editor that the upload failed
     header("HTTP/1.1 500 Server Error");
